d4605855e29fbcbad2c35757fe037de706e290e9
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-11 23:07:17 -0300

    testing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Band band = new Band("band name");
+
+Music music = new Music("music name", 217, band);
+
+Album album = new Album("album name", new List<Music>() { music });
