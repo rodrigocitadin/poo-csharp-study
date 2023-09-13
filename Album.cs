b56d2d97eac31c0df2ba9afd67557bed61b04323
rodrigocitadin <citadinrodrigo@gmail.com>
2023-09-13 16:19:10 -0300

    improving the class functions

--- Album.cs
+++ Album.cs
@@ -9,4 +9,19 @@ class Album
     public string Name { get; }
     public List<Music> Musics { get; }
     public int DurationInSeconds => Musics.Sum(m => m.DurationInSeconds);
+
+    public void AddMusic(Music music)
+    {
+        Musics.Add(music);
+    }
+
+    public void RemoveMusic(Music music)
+    {
+        Musics.Remove(music);
+    }
+
+    public void RemoveMusicById(int index)
+    {
+        Musics.RemoveAt(index);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,3 +3,13 @@
 Music music = new Music("music name", 217);
 
 Album album = new Album("album name", new List<Music>() { music });
+
+System.Console.WriteLine($"{album.Musics.Count}");
+System.Console.WriteLine($"{album.DurationInSeconds}");
+
+Music otherMusic = new Music("other music", 309);
+
+album.AddMusic(otherMusic);
+
+System.Console.WriteLine($"{album.Musics.Count}");
+System.Console.WriteLine($"{album.DurationInSeconds}");
